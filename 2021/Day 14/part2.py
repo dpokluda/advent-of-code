32527bb26fc5ac934ccc0b4a76906cc2adccbe46
David Pokluda <46501915+dpokluda@users.noreply.github.com>
2021-12-14 11:40:09 -0800

    Day 14 part 2

--- 2021/Day 14/part2.py
+++ 2021/Day 14/part2.py
@@ -1,48 +1,34 @@
-rules = {}
-
-
-def update_counts(character, counts):
-    if character in counts:
-        counts[character] += 1
+def update_counts(key, counts, count):
+    if key in counts:
+        counts[key] += count
     else:
-        counts[character] = 1
-
-
-def calculate_counts(first, second, level, counts):
-    key = rules.get(first + second)
-    update_counts(key, counts)
-    if level < 30:
-        calculate_counts(first, key, level + 1, counts)
-        calculate_counts(key, second, level + 1, counts)
+        counts[key] = count
 
 
 def run():
-    lines = [x.strip() for x in open('test.txt', 'r').readlines()]
+    lines = [x.strip() for x in open('input.txt', 'r').readlines()]
     template = [x for x in lines[0]]
+    rules = {}
     for index in range(2, len(lines)):
         items = lines[index].split(' -> ')
         rules[items[0]] = items[1]
 
-    counts = {}
-    # for character in template:
-    #     update_counts(character, counts)
-
-    patterns = {}
-    for index, (key, count) in enumerate(rules.items()):
-        print(index)
-        rule_counts = {}
-        calculate_counts(key[0], key[1], 1, rule_counts)
-        patterns[key] = rule_counts
-
+    pair_counts = {}
     for index in range(0, len(template) - 1):
-        update_counts(template[index], counts)
-        pattern_counts = patterns.get(template[index] + template[index + 1])
-        for index, (key, count) in enumerate(pattern_counts.items()):
-            if key in counts:
-                counts[key] += count
-            else:
-                counts[key] = count
-    counts[template[len(template)-1]] += 1
+        update_counts(template[index] + template[index + 1], pair_counts, 1)
+
+    for step in range(0, 40):
+        new_pair_counts = {}
+        for index, (key, count) in enumerate(pair_counts.items()):
+            insert = rules.get(key)
+            update_counts(key[0] + insert, new_pair_counts, count)
+            update_counts(insert + key[1], new_pair_counts, count)
+        pair_counts = new_pair_counts
+
+    counts = {}
+    for index, (key, count) in enumerate(pair_counts.items()):
+        update_counts(key[1], counts, count)
+    counts[template[0]] -= 1
 
     max_count = -1
     min_count = -1
